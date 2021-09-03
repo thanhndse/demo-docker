8b3c837a68ade472dc45d6e4d1bba6b77f25b179
Thanh Nguyen1 <thanh_nguyen1@epam.com>
2021-09-03 13:53:18 +0700

    first

---
+++ src/main/java/com/thanhnd/demodocker/DemoDockerApplication.java
@@ -0,0 +1,21 @@
+package com.thanhnd.demodocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoDockerApplication {
+
+	@GetMapping("/test")
+	public String test(){
+		return "Thanh dep trai";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDockerApplication.class, args);
+	}
+
+}
