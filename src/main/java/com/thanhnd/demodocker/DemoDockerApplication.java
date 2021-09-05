14f0cf84dacd16c047336f2d54222d0a2360b67b
Thanh Nguyen1 <thanh_nguyen1@epam.com>
2021-09-05 23:14:24 +0700

    change

--- src/main/java/com/thanhnd/demodocker/DemoDockerApplication.java
+++ src/main/java/com/thanhnd/demodocker/DemoDockerApplication.java
@@ -11,7 +11,7 @@ public class DemoDockerApplication {
 
 	@GetMapping("/test")
 	public String test(){
-		return "This is my first deploy app";
+		return "Hom nay la thu 2";
 	}
 
 	public static void main(String[] args) {
